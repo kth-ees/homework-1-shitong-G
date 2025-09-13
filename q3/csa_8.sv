3edb01cce91f7366a12e94445f1d3bae061a8f11
shitong-G <jason.guo@sjtu.edu.cn>
2025-09-13 17:11:09 +0200

    update: q3

--- q3/csa_8.sv
+++ q3/csa_8.sv
@@ -3,7 +3,29 @@ module csa_8 (
   output logic [7:0] sum,
   output logic carry
 );
-  // …
+  // �
+  logic c3;
+  logic [7:0] sum_;
+  adder_4 a0(
+    .A(a[3:0]),
+    .B(b[3:0]),
+    .carry(c3),
+    .sum(sum[3:0])
+  );
+  adder_4 a1(
+    .A(a[7:4]),
+    .B(b[7:4]),
+    .carry(c07),
+    .sum(sum_[3:0])
+  );
+  adder_4 a2(
+    .A(a[7:4]),
+    .B(b[7:4] + 1),
+    .carry(c17),
+    .sum(sum_[7:4])
+  );
+  assign carry = c3 ? c17 : c07;
+  assign sum[7:4] = c3 ? sum_[7:4] : sum_[3:0];
   // Add your description here
   // …
 endmodule
--- q3/csa_8_tb.sv
+++ q3/csa_8_tb.sv
@@ -3,9 +3,30 @@ module csa_8_tb;
   // Inputs
   logic [7:0] a;
   logic [7:0] b;
-  logic [7:0] sum,
+  logic [7:0] sum;
   logic carry;
 
+  csa_8 DUT(
+    .a(a),
+    .b(b),
+    .sum(sum),
+    .carry(carry)
+  );
+
+  initial begin
+    a = 8'b0; b = 8'b0;
+    #10 a = 8'b0000_0000; b = 8'b0000_0000;
+    #10 a = 8'b0000_1000; b = 8'b0000_1000;
+    #10 a = 8'b0000_1011; b = 8'b0000_0011;
+    #10 a = 8'b0010_1111; b = 8'b1000_1111;
+    #10 a = 8'b1111_1111; b = 8'b1111_1111;
+    #10 $finish;
+  end
+
+  initial begin
+    $monitor("Time=%0t | a=%b b=%b sum=%b carry=%b", $time, a, b, sum, carry);
+  end
   // complete
-  
+	
+
 endmodule
